291ae5432f852ecac9a951dcb824ce33fca3edfa
Rich Hanes <georgebastille@gmail.com>
2020-08-23 19:32:28 +0100

    First static dataframe

--- hello_server.py
+++ hello_server.py
@@ -1,5 +1,6 @@
 import os
 
+import pandas as pd
 from flask import Flask, render_template
 
 # Set environment variables
@@ -15,7 +16,11 @@ def landing():
 
 @app.route("/<query>")
 def run_query(query):
-    return render_template("greetings.html", name=query)
+    df = pd.read_csv("./data/Repossession-2019-07.csv").to_dict("records")
+    return render_template("greetings.html", name=query, dataframe=df)
+
+
+# Next up:  https://stackoverflow.com/questions/9198334/how-to-build-up-a-html-table-with-a-simple-for-loop-in-jinja2
 
 
 if __name__ == "__main__":
